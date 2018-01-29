dc592422d2545cd1c24b53ecf0fa4e4eb37bc7a5
Dan Swartz <dan.swartz@anitian.com>
2018-01-29 14:47:37 -0500

    update-cookbooks.sh fixes depends in kibana/metadata.json

--- update-cookbooks.sh
+++ update-cookbooks.sh
@@ -3,3 +3,4 @@ librarian-chef install
 for book in `ls cookbooks`; do
   rm -rf $book && mv cookbooks/$book .
 done
+sed  -i.bak 's/}}$/},"depends":"apache2"}/' kibana/metadata.json && rm kibana/metadata.json.bak
